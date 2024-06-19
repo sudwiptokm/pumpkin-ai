34c292f8094a6941df392aa904a409998bd81158
sudwiptokm <sudwiptokmondal@gmail.com>
2024-06-20 04:01:20 +0600

    fix: auth middleware updated

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,25 @@
 import { authMiddleware } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
 
 export default authMiddleware({
   publicRoutes: ["/", "/auth(.*)", "/portal(.*)", "/images(.*)"],
   ignoredRoutes: ["/chatbot"],
+  afterAuth: (auth, req) => {
+    const url = req.nextUrl.clone();
+
+    // Check if user is on the sign-in page and authenticated
+    if (req.nextUrl.pathname.startsWith("/auth/sign-in") && auth.sessionId) {
+      url.pathname = "/dashboard";
+      return NextResponse.redirect(url);
+    }
+
+    // If the user tries to access /dashboard without being authenticated
+    if (req.nextUrl.pathname === "/dashboard" && !auth.sessionId) {
+      url.pathname = "/auth/sign-in";
+      url.searchParams.set("redirect_url", "/dashboard");
+      return NextResponse.redirect(url);
+    }
+  },
 });
 
 export const config = {
